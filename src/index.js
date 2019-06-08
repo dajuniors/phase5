ed63e16528649c935b84f451d303f4f8558f8bd3
Naveen Janarthanan <navjanar@uw.edu>
2019-06-08 15:38:39 -0700

    Popup shows building name" git push

--- src/index.js
+++ src/index.js
@@ -168,6 +168,41 @@ map.loadImage("https://img.icons8.com/color/24/000000/marker.png", function(erro
 
 
 
+// from site
+map.on('click', 'testing', function (e) {
+  console.log('hello')
+  var coordinates = e.features[0].geometry.coordinates.slice();
+
+  // create html for popup
+  let btn = document.createElement('button');
+  btn.textContent = 'test';
+  var description = e.features[0].properties.name
+   
+  // Ensure that if the map is zoomed out such that multiple
+  // copies of the feature are visible, the popup appears
+  // over the copy being pointed to.
+  while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+  coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+  }
+
+  new mapboxgl.Popup()
+  .setLngLat(coordinates)
+  .setHTML(description)
+  .addTo(map);
+  });
+
+  // Change the cursor to a pointer when the mouse is over the places layer.
+map.on('mouseenter', 'testing', function () {
+  map.getCanvas().style.cursor = 'pointer';
+  });
+   
+  // Change it back to a pointer when it leaves.
+  map.on('mouseleave', 'testing', function () {
+  map.getCanvas().style.cursor = '';
+  });
+
+
+
 function onErrorCurrentPos(error) {
     console.error(error);
 }
